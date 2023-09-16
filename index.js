3cf68804f63842a7f0d5d75fcde7662f512b26bc
mohan7401647399 <mtm.kcs@gmail.com>
2023-09-16 15:44:30 +0530

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+function foo() {
+    var getId = document.getElementById('fname').value;
+    var getId2 = document.getElementById('midname').value;
+    var getId3 = document.getElementById('lname').value;
+    var getId4 = document.getElementById('email').value;
+    var getId5 = document.getElementById('pincode').value;
+    console.log(getId,getId2,getId3,getId4,getId5);
+}
